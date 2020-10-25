dda7301d83237210a319f7aeea741f03f6aa3eca
ehdwls9797 <rlaehdwlsant@naver.com>
2020-10-25 23:02:45 +0900

    10/25 게시판관련 주석 완료

--- src/main/java/com/spring/biz/SampleService.java
+++ src/main/java/com/spring/biz/SampleService.java
@@ -8,26 +8,37 @@ import com.spring.biz.vo.BoardVO;
 
 public interface SampleService {
 	
+	//카테고리 리스트
 	List<BoardCategoryVO> categoryList();
 	
+	//보드게시판에서 카테고리 이름빼오기
 	BoardCategoryVO categoryName (int categoryNum);
 	
+	//게시판 리스트 뽑아오기
 	List<BoardVO> boardList(BoardVO boardVO);
 	
+	//글쓰기
 	int insertBoard(BoardVO boardVO);
 	
+	//상세보기
 	BoardVO detail(BoardVO boardVO);
 	
+	//글삭제
 	int deleteBoard(BoardVO boardVO);
 	
+	//조회수
 	int readCnt(int boardNum);
 	
+	//글수정
 	int updateBoard(BoardVO boardVO);
 	
+	//댓글입력
 	int insertComment(BoardCommentVO boardCommentVO);
 	
+	//댓글 리스트 뽑아오기
 	List<BoardCommentVO> selectComment(int boardNum);
 	
+	//댓글 총 갯수 출력
 	int commentCnt(int boardNum);
 	
 	
